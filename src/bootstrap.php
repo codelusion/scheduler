ac9b473a03dbb9881dcffab6c63927b0926d7244
codelusion <ash@aisaac.io>
2016-01-23 11:06:52 -0600

    Container initialization in bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,10 +3,29 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use \Scheduler\Repositories\UserSQLiteRepository;
+use Scheduler\Services\UserService;
+use Scheduler\Validation\UserValidator;
+use Scheduler\Validation\ValidationHelper;
+use Symfony\Component\Validator\ValidatorBuilder;
 
 $app = new Proton\Application();
-$app['database']  = '../DB/scheduler.db';
 
-$app['userRepository'] =  function() use ($app) {
+$app['database'] = __DIR__.'/../DB/scheduler.db';
+
+$app['user_repository'] = function () use ($app) {
     return new UserSQLiteRepository($app['database']);
+};
+
+$app['validation_helper'] = function () use ($app) {
+    $validatorBuilder = new ValidatorBuilder();
+    $validator = $validatorBuilder->getValidator();
+    return new ValidationHelper($validator);
+};
+
+$app['user_validator'] = function () use ($app) {
+    return new UserValidator($app['validation_helper']);
+};
+
+$app['user_service'] = function () use ($app) {
+    return new UserService($app['user_repository'], $app['user_validator']);
 };
